992c6b5346e2aa5b4b315c3e807a607be92cf6e1
nexdroidwx <grey.nexdroid@gmail.com>
2026-01-21 15:30:19 +0530

    Update gen_scripts.py

--- gen_scripts.py
+++ gen_scripts.py
@@ -1,69 +1,95 @@
 import os
 import sys
 
-if len(sys.argv) < 3:
-    print("Usage: gen_scripts.py <device_code> <path_to_images_dir>")
-    sys.exit(1)
+def generate_bat(device_code, images_dir):
+    # The header you requested
+    bat_content = f"""@echo off
+cd %~dp0
+set fastboot=bin\\windows\\fastboot.exe
+if not exist %fastboot% echo %fastboot% not found. & pause & exit /B 1
+echo Waiting for device...
+set device=
+for /f "tokens=2" %%A in ('%fastboot% getvar product 2^>^&1 ^| findstr "\\<product:"') do set device=%%A
+if "%device%" equ "" echo Your device could not be detected. & pause & exit /B 1
+echo Your device: %device%
+if "%device%" neq "{device_code}" if "%device%" neq "{device_code}in" echo Compatible devices: {device_code}, {device_code}in & pause & exit /B 1
 
-DEVICE = sys.argv[1]
-IMG_DIR = sys.argv[2]
+echo Your device will be flashed and the data partition will be formatted.
+echo You will lose your apps, settings and files on internal storage.
+echo Continue if you are flashing a custom ROM for the first time or downgrading.
+set /p choice=Do you want to continue? [y/N] 
+if /i "%choice%" neq "y" exit /B 0
 
-# Detect actual images present
-files = os.listdir(IMG_DIR)
-images = [f for f in files if f.endswith(".img")]
+echo ##############################################################
+echo Please wait. The device will reboot once flashing is complete.
+echo ##############################################################
+%fastboot% set_active a
+"""
 
-# Flashing Order
-BOOT_IMGS = ["boot.img", "vendor_boot.img", "dtbo.img", "recovery.img", "init_boot.img"]
-VBMETA_IMGS = ["vbmeta.img", "vbmeta_system.img", "vbmeta_vendor.img"]
-SUPER_IMG = "super.img"
+    # Priority partitions (Firmware)
+    # We map common names to your script's specific order/naming
+    firmware_order = [
+        "abl", "bluetooth", "countrycode", "devcfg", "dsp", "dtbo", "featenabler",
+        "hyp", "imagefv", "keymaster", "modem", "qupfw", "rpm", "tz", 
+        "uefisecapp", "vbmeta", "vbmeta_system", "vbmeta_vendor", "xbl", "xbl_config",
+        "boot", "init_boot", "vendor_boot", "cust"
+    ]
 
-HEADER_BAT = f"@echo off\ntitle NexDroid Gooner Installer - {DEVICE}\ncolor 0b\necho NEXDROID GOONER - {DEVICE}\necho.\nfastboot devices\necho.\n"
-HEADER_SH = f"#!/bin/bash\necho 'NEXDROID GOONER - {DEVICE}'\nfastboot devices\n"
-
-def get_cmds(os_type, wipe):
-    cmds = []
-    # 1. Safety & FRP
-    cmds.append("fastboot erase frp")
-    cmds.append("fastboot erase misc")
-    cmds.append("fastboot erase metadata")
+    files = os.listdir(images_dir)
     
-    # 2. Firmware
-    for img in BOOT_IMGS:
-        if img in images: cmds.append(f"fastboot flash {img.split('.')[0]} images/{img}")
+    # 1. Flash Firmware (A/B slots)
+    for part in firmware_order:
+        img_name = f"{part}.img"
+        if img_name in files:
+            # Special handling for cust (usually not A/B)
+            if part == "cust":
+                bat_content += f"%fastboot% flash {part} images\\{img_name}\n"
+            else:
+                bat_content += f"%fastboot% flash {part}_ab images\\{img_name}\n"
+
+    # 2. Flash Super (The Big One)
+    if "super.img" in files:
+        bat_content += "%fastboot% flash super images\\super.img\n"
+
+    # 3. Footer
+    bat_content += """%fastboot% erase metadata
+%fastboot% erase userdata
+%fastboot% reboot
+pause
+"""
     
-    # 3. Vbmeta (Disable checks)
-    for img in VBMETA_IMGS:
-        if img in images: 
-            cmds.append(f"fastboot --disable-verity --disable-verification flash {img.split('.')[0]} images/{img}")
+    with open("flash_all.bat", "w") as f:
+        f.write(bat_content)
 
-    # 4. Other firmware (dsp, modem, etc)
-    for img in images:
-        if img not in BOOT_IMGS and img not in VBMETA_IMGS and img != SUPER_IMG:
-            part = img.split('.')[0]
-            cmds.append(f"fastboot flash {part} images/{img}")
-
-    # 5. Super
-    if SUPER_IMG in images:
-        cmds.append(f"fastboot flash super images/{SUPER_IMG}")
-
-    # 6. Wipe Option
-    if wipe: cmds.append("fastboot -w")
+def generate_sh(device_code, images_dir):
+    # Minimal Linux/Mac version
+    sh_content = f"""#!/bin/bash
+fastboot=./bin/linux/fastboot
+chmod +x $fastboot
+$fastboot getvar product 2>&1 | grep "^product: {device_code}" || {{ echo "Wrong device!"; exit 1; }}
+$fastboot set_active a
+"""
+    # Simplified loop for linux
+    files = os.listdir(images_dir)
+    for f in files:
+        if f.endswith(".img") and f != "super.img":
+            name = f.replace(".img", "")
+            if name == "cust":
+                sh_content += f"$fastboot flash {name} images/{f}\n"
+            else:
+                sh_content += f"$fastboot flash {name}_ab images/{f}\n"
     
-    cmds.append("fastboot reboot")
-    if os_type == "win": cmds.append("pause")
-    return cmds
+    if "super.img" in files:
+        sh_content += "$fastboot flash super images/super.img\n"
 
-# Generate Files
-with open("flash_clean.bat", "w") as f:
-    f.write(HEADER_BAT + "echo [!] WIPING DATA...\n" + "\n".join(get_cmds("win", True)))
+    sh_content += "$fastboot erase metadata\n$fastboot erase userdata\n$fastboot reboot\n"
 
-with open("flash_dirty.bat", "w") as f:
-    f.write(HEADER_BAT + "echo [*] Dirty Flash (Keeping Data)...\n" + "\n".join(get_cmds("win", False)))
+    with open("flash_all.sh", "w") as f:
+        f.write(sh_content)
 
-with open("flash_clean.sh", "w") as f:
-    f.write(HEADER_SH + "read -p '[!] WIPING DATA. Press Enter...' \n" + "\n".join(get_cmds("linux", True)))
-
-with open("flash_dirty.sh", "w") as f:
-    f.write(HEADER_SH + "echo '[*] Dirty Flash (Keeping Data).'\n" + "\n".join(get_cmds("linux", False)))
-
-print("✅ Flashing scripts generated.")
+if __name__ == "__main__":
+    dev_code = sys.argv[1]
+    img_dir = sys.argv[2]
+    generate_bat(dev_code, img_dir)
+    generate_sh(dev_code, img_dir)
+    print(f"✅ Scripts generated for device: {dev_code}")
